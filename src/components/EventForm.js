fd816629af275c63963674928ff28827cfa78540
devmullets <devmullets@gmail.com>
2020-06-08 14:20:02 -0400

    update EventForm

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,4 +1,20 @@
 import React, { Component } from 'react';
+import { Form, Input, Select, Dropdown } from 'semantic-ui-react'
+
+const options = [
+  { key: 'm', text: 'Male', value: 'male' },
+  { key: 'f', text: 'Female', value: 'female' },
+  { key: 'o', text: 'Other', value: 'other' },
+  ]
+
+const eventOptions = [
+  { key: 'nf', value: 'nf', text: 'Nightfall'},
+  { key: 'gos', value: 'gos', text: 'Garden of Salvation'},
+  { key: 'lw', value: 'lw', text: 'Last Wish'},
+  { key: 'levi', value: 'levi', text: 'Leviathan'},
+  { key: 'sotp', value: 'sotp', text: 'Scourge of the Past'},
+  
+]
 
 class EventForm extends Component {
   state = {
@@ -9,40 +25,49 @@ class EventForm extends Component {
     description: '',
   }
 
-  onFormChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value})
+  onFormChange = (e, { name, value }) => {
+    this.setState({ [name]: value})
   }
 
-  onDateChange = (date,dateString) => {
-    // function(date: moment, dateString: string)
-    // console.log(date, dateString)
-    this.setState({ date: dateString })
-
-  }
-  onTimeChange = (time, timeString) => {
-    // function(date: moment, dateString: string)
-    this.setState({ time: timeString })
-    
+  handleSubmit = () => {
+    //
   }
 
-  onEventChange = (value) => {
-    this.setState({ event: value })
-  }
-
-  onCheckedChange = (e) => {
-    this.setState({ helper: e.target.checked })
-  }
-
-  // handleSubmit = () => {
-  //   // form doesn't pass values for some reason, temp pass
-  //   // current state values instead
-  //   alert('hi buddy')
-  // }
+  
 
   render() {
     return (
       <>
-       form_here
+      <br />
+       <Form onSubmit={(e) => this.props.addNewEvent(this.state)}>
+        <Form.Group widths='equal'>
+          <Form.Field>
+            <label>Middle name</label>
+            <Input fluid placeholder='Middle name' />
+          </Form.Field>
+          <Form.Field
+            control={Select}
+            label='Gender'
+            options={options}
+            placeholder='Gender'
+          />
+          <Form.Field
+            name="event"
+            label='Select Event'
+            control={Dropdown}
+            placeholder='Select Event'
+            search
+            selection
+            options={eventOptions}
+            onChange={this.onFormChange}
+          />
+          <Form.Field
+            control={Input}
+            label='Event Notes'
+            placeholder='Flawless run etc'
+          />
+        </Form.Group>
+      </Form>
       </>
     );
   }
